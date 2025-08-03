93063cef66ddc07812e2b9a061d0c0109119294d
OishiiShishamo <shishamodesu@outlook.jp>
2025-08-03 20:51:10 +0900

    heiretsuka

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include "main.h"
 
+#include <algorithm>
+#include <execution>
+#include <numeric>
 #include <iostream>
 #include <cmath>
 #include <vector>
@@ -52,23 +55,27 @@ namespace emvg {
 
     void Run(int end_time) {
         int time = 0;
+        std::array<int, kWidth * kHeight> indices;
+        std::iota(indices.begin(), indices.end(), 0);
         while (time != end_time) {
             ClearCanvas();
-            for (int x = 0; x < kWidth; x++) {
-                for (int y = 0; y < kHeight; y++) {
-                    double dx = x - kWidth / 2.0;
-                    double dy = y - kHeight / 2.0;
-                    double r = std::sqrt(dx * dx + dy * dy);
-                    double angle = std::atan2(dy, dx);
-                    double phase = angle + r * 0.05 + time * 0.05;
-                    double diff = std::abs(std::sin(phase) * std::exp(-r * 0.02));
-                    if (diff < 0.5) {
-                        DrawPixelAA(x, y, GamingColor(time), diff, 0.5, BlendType::kBlendAdd);
-                    }
+            std::for_each(std::execution::par, indices.begin(), indices.end(), [&](int i) {
+                int x = i % kWidth;
+                int y = i / kWidth;
+                double dx = x - kWidth / 2.0;
+                double dy = y - kHeight / 2.0;
+                double r = std::sqrt(dx * dx + dy * dy);
+                double angle = std::atan2(dy, dx);
+                double phase = angle + r * 0.05 + time * 0.05;
+                double diff = std::abs(std::sin(phase) * std::exp(-r * 0.02));
+                if (diff < 0.5) {
+                    DrawPixelAA(x, y, GamingColor(time), diff, 0.5, BlendType::kBlendAdd);
                 }
-            }
+            });
             stbi_write_bmp(("./output/" + std::to_string(time) + ".bmp").c_str(), kWidth, kHeight, 3, canvas.data());
             time++;
+			if (time != 0) std::cout << "Progress: " << static_cast<double>(time) / static_cast<double>(end_time) * 100 << "%" << std::endl;
+			else std::cout << "Progress: 0%" << std::endl;
         }
     }
 }
